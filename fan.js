0b6e65b6dbeadc977bc79f0c3eab327184a9b26f
Swongliver <81005472+Swongliver@users.noreply.github.com>
2022-02-07 17:29:33 +0800

    Add files via upload

---
+++ fan.js
@@ -0,0 +1,56 @@
+status1="";
+objects=[];
+function preload(){
+    
+    image1=loadImage("fan.png")
+
+}
+function setup(){
+    canvas=createCanvas(250,350)
+    canvas.center()
+    objectDetector=ml5.objectDetector("cocossd", modelLoaded)
+    }
+    function modelLoaded(){
+        status1=true
+        console.log('model is loaded')
+        objectDetector.detect(image1, gotResult)
+    }
+    function gotResult(error,results){
+        if (error) {
+            console.log("error")
+        }else{
+    console.log(results)
+    objects=results;
+        }
+    }
+
+function draw(){
+    image(image1, 0,0,300,300)
+    if (status1 != "") {
+        document.getElementById("detect_status").innerHTML="Status: Objects Detected"
+        for ( i=0 ;i < objects.length; i++) {
+            fill("red")   
+            percentage=floor(objects[i].confidence*100)
+            text(objects[i].label+" "+percentage+"%",objects[i].x, objects[i].y)
+            noFill()
+            stroke("red")
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height)         
+        }
+    }
+}
+
+function back(){
+    window.location="index.html"
+    }
+    function fan(){
+        window.location="fan.html"
+        }
+        function chair(){
+            window.location="chair.html"
+            }
+            function bottle(){
+                window.location="bottle.html"
+                }
+                function bed(){
+                    window.location="bed.html"
+                    }
---
+++ main.js
@@ -0,0 +1,16 @@
+
+function back(){
+window.location="index.html"
+}
+function fan(){
+    window.location="fan.html"
+    }
+    function chair(){
+        window.location="chair.html"
+        }
+        function bottle(){
+            window.location="bottle.html"
+            }
+            function bed(){
+                window.location="bed.html"
+                }
